41590db24de196bbfb3c3684ba371e41abd751d7
pedro-bitencourt <pedro.bitencourt@u.northwestern.edu>
2025-03-12 19:13:04 -0500

    new battery.xml

--- finalize.py
+++ finalize.py
@@ -7,10 +7,10 @@ setup_logging(level = "info")
 from src.finalization_module import SimulationFinalizer, visualize
 from src.constants import BASE_PATH
 
-
 simulation_name: str = "factor_compartir_gas_hi_wind"
-
 costs_path: Path = BASE_PATH / "code/cost_data/gas_high_wind_cost.json"
+
+
 simulation_folder: Path = BASE_PATH / "sim" / simulation_name
 x_variable: dict[str, str] = {"name": "factor_compartir", "label": "Factor Compartir"}
 endogenous_participants: list[str] = ["solar", "wind", "thermal"]
