824dbd8d8f335e2c55f148acdbbcea4981fd302d
pedro-bitencourt <pedro.bitencourt@u.northwestern.edu>
2025-03-16 13:48:29 -0500

    sync: quest

--- scripts/battery_gas.py
+++ scripts/battery_gas.py
@@ -5,12 +5,12 @@ from src.comparative_statics_module import ComparativeStatics
 from src.utils.logging_config import setup_logging
 
 
-setup_logging(level="debug")
+setup_logging(level="info")
 
 # Input parameters
-name: str = 'factor_compartir_coal'
-xml_basefile: str = '/projects/p32342/code/xml/salto_capacity.xml'
-costs_path: str = '/projects/p32342/code/cost_data/coal.json'
+name: str = 'battery_gas'
+xml_basefile: str = '/projects/p32342/code/xml/battery_gas.xml'
+costs_path: str = '/projects/p32342/code/cost_data/gas.json'
 
 general_parameters: dict = {
     'daily': True,
@@ -18,18 +18,18 @@ general_parameters: dict = {
     'cost_path': costs_path,
     'annual_interest_rate': 0.0,
     'email': 'pedro.bitencourt@u.northwestern.edu',
-    'slurm': {'run': {'time': 0.55}}
+    'slurm': {'run': {'time': 0.75, 'mailtype': 'FAIL'}}
 }
 
-exog_grid: list[float] = [0.01, 0.1, 0.25, 0.4, 0.5, 0.6, 0.75, 1]
+exog_grid: list[float] = [1, 2, 5, 10, 25, 50]
 
 exogenous_variables: dict[str, dict] = {
-    'factor_compartir': {'grid': exog_grid},
+    'battery_factor': {'grid': exog_grid},
 }
 endogenous_variables: dict[str, dict] = {
-    'wind_capacity': {'initial_guess': 1000},
-    'solar_capacity': {'initial_guess': 1000},
-    'thermal_capacity': {'initial_guess': 1600}
+    'wind_capacity': {'initial_guess': 2200},
+    'solar_capacity': {'initial_guess': 1500},
+    'thermal_capacity': {'initial_guess': 1500}
 }
 
 variables: dict[str, dict] = {
@@ -46,8 +46,8 @@ comparative_statics = ComparativeStatics(
 
 
 # Submit the solver jobs
-comparative_statics.prototype()
-#comparative_statics.submit_solvers()
+# comparative_statics.prototype()
+# comparative_statics.submit_solvers()
 # Submit the processing job
 #comparative_statics.submit_processing()
-#comparative_statics.process()
+comparative_statics.process()
--- src/run_module.py
+++ src/run_module.py
@@ -68,15 +68,12 @@ class Run:
 
     def capacities(self):
         participants: list[str] = self.general_parameters.get(
-            'endogenous_participants', PARTICIPANTS_ENDOGENOUS_DEFAULT
+            'participants', PARTICIPANTS_DEFAULT
         )
         # Create a dictionary with the capacities
         capacities = {participant: self.variables[f"{participant}_capacity"]
                       for participant in participants}
-        if 'salto_capacity' in self.variables.keys():
-            capacities['salto'] = self.variables['salto_capacity']
-        else:
-            capacities['salto'] = 810
+
         return capacities
 
     def log_run(self):
@@ -139,13 +136,13 @@ class Run:
             # Check if files exist
             missing_files = self.get_missing_files(self.paths['sim'], complete)
             if missing_files:
-                logger.debug(
+                logger.info(
                     "Run %s is missing files: %s", self.name, missing_files)
                 return False
         else:
-            logger.debug('No sim folder found for run %s in folder %s',
-                         self.name,
-                         self.paths['subfolder'])
+            logger.info('No sim folder found for run %s in folder %s',
+                        self.name,
+                        self.paths['subfolder'])
             return False
 
         return True
@@ -258,7 +255,7 @@ class Run:
         header = slurm_header(run_config, job_name, slurm_path)
 
         memory = run_config.get('memory', MEMORY_REQUESTED)
-        temp_folder_path = f"/tmp/pdm6134/{self.name}"
+        temp_folder_path = f"/projects/p32342/temp/{self.name}"
         temp_folder_path_windows = temp_folder_path.replace('/', '\\')
         wine_path = self.general_parameters.get(
             'wine_path', '/projects/p32342/software/.wine')
